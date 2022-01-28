d742087321ead7ef096ad6856b08728492a94145
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2022-01-28 11:00:03 +0100

    js + revoir fonts probleme serv iut

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ let colors = ['rgb(201, 0, 0)', 'rgb(0, 24, 201)', 'rgb(24, 201, 0)', 'rgb(232, 
 function colorChange() {
     let e = document.getElementById('e');
     let id = Math.floor(Math.random() * 4);
+
     e.style.transition = 'color 250ms ease';
     e.style.color = colors[id];
+}
+
+
+
+function nextInt(n) {
+    if (Number(n) == 3) {
+        return 0;
+    } else {
+        return Number(n)+1;
+    }
 }
