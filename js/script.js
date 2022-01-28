b2889b810d2eb918054bba4683347b767d229083
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2022-01-28 10:26:49 +0100

    refonte

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let colors = ['rgb(201, 0, 0)', 'rgb(0, 24, 201)', 'rgb(24, 201, 0)', 'rgb(232, 214, 0)']
+
+
+function colorChange() {
+    let e = document.getElementById('e');
+    let id = Math.floor(Math.random() * 4);
+    e.style.transition = 'color 250ms ease';
+    e.style.color = colors[id];
+}
