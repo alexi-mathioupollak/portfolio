22d7bc7e631b14e827fa24e34c1bf2a977bdf965
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2022-01-28 15:56:02 +0100

    JS + merciBaptiste

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-let colors = ['rgb(201, 0, 0)', 'rgb(0, 24, 201)', 'rgb(24, 201, 0)', 'rgb(232, 214, 0)']
+let colors = ['#007D59', '#EBC238', '#A2536A']
 
 
 function colorChange() {
-    let e = document.getElementById('e');
-    let id = Math.floor(Math.random() * 4);
+    let e = document.getElementById('pf');
+    let id = Math.floor(Math.random() * 3);
 
-    e.style.transition = 'color 250ms ease';
+    e.style.transition = 'color 500ms ease';
     e.style.color = colors[id];
 }
-
-
 
 function nextInt(n) {
     if (Number(n) == 3) {
@@ -18,3 +16,52 @@ function nextInt(n) {
         return Number(n)+1;
     }
 }
+
+const ratio = 0
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            if (entry.target === document.querySelector('.lastname')) {
+                entry.target.style.transitionDelay = '.4s'
+            }
+            if (entry.target === document.querySelector('.portfolio')) {
+                entry.target.style.transitionDelay = '.6s'
+                entry.target.style.transform = 'translateY(30px)'
+            } else if (entry.target === document.querySelector('.text_aboutme')) {
+                entry.target.style.transitionDelay = '.4s';
+            }
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        } else {
+            console.log('invisible')
+        }
+        console.log(entry.intersectionRatio)
+    })
+    console.log('handleIntersect')
+}
+
+const animate = document.querySelectorAll('.reveal')
+console.log(animate)
+const observer = new IntersectionObserver(handleIntersect, options)
+
+animate.forEach(function (r) {
+    observer.observe(r)
+})
+
+function reduceMenu() {
+    let menu = document.getElementById('navbar')
+    if (window.offsetTop > 10) {
+        menu.classList.remove('navbar')
+        menu.classList.add('navbar_scroll')
+    } else {
+        console.log('non')
+    }
+}
+
+let interval = setInterval(colorChange, 1000);
